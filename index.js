d866b2edb31db3427f123a6f250220c4010e895b
Julian Engel <julian13engel@gmail.com>
2018-08-28 14:45:07 +0300

    Version 0.5

---
+++ index.js
@@ -0,0 +1,10 @@
+let bool = false;
+
+if (bool){
+  document.body.style.backgroundColor = "#4cd137";
+  document.getElementById("text").innerHTML =  "Tak";
+} else{
+  document.body.style.backgroundColor = "#e84118";
+  document.getElementById("text").innerHTML = "Nie"
+
+}
