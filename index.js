2ae136e4c5fe0b85ea04374eb23fbae2baa0ef4d
Julian Engel <julianengel@users.noreply.github.com>
2018-10-31 15:47:35 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
 let bool = false;
 
+var d = new Date();
+var first_week = d.getDate();
+var the_day = d.getDay();
+
+if(first_week <= 7 && the_day == 0)
+{
+  // It's the first Sunday of the month
+  bool = true;
+}
+
+
 if (bool){
   document.body.style.backgroundColor = "#4cd137";
   document.getElementById("text").innerHTML =  "Tak";
